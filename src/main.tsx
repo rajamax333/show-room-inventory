20ccb5c43a3d6347642f407e8b41512d2322e01a
rajnarayanan <rnarayanan@tekion.com>
2025-07-26 14:41:15 +0530

    chnages of login and dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,7 @@ import {Routes} from './routes/routes'
 import { ToastContainer } from 'react-toastify';
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
-
+  // Enable MSW in both development and production
   const { worker } = await import('./mocks/browser')
 
   // Start the worker and wait for it to be ready
